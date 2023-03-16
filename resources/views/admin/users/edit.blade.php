41815bb7659c7b770dc7e98e98a874f4cc1ef277
iVampireSP.com <im@ivampiresp.com>
2023-03-16 22:43:46 +0800

    改进 不自动补全

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -64,7 +64,7 @@
             <div class="input-group mb-3">
                 <button class="btn btn-outline-secondary" type="button" id="new_password_btn">随机密码</button>
                 <input id="new_password" type="password" name="password" class="form-control" placeholder="新的密码（留空不会设置）"
-                       aria-label="新的密码（留空不会设置" aria-describedby="new_password_btn">
+                       aria-label="新的密码（留空不会设置" aria-describedby="new_password_btn" autocomplete="false">
             </div>
 
         </div>
