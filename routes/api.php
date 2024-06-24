3e7b5fb60d15933e48099be43a8f7c542defdd9b
ivamp <im@ivampiresp.com>
2024-06-24 17:39:14 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -14,16 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('user', [UserController::class, 'user'])->middleware('scopes:user')->name('openid.userinfo');
+Route::get('user', [UserController::class, 'user'])->name('openid.userinfo');
 
 Route::get('real-name', [UserController::class, 'realName'])
     ->middleware('scopes:realname');
 
-// 状态
-Route::get('status', [UserController::class, 'status'])
-    ->middleware('scopes:user');
-// 创建或更新状态
-Route::post('status', [UserController::class, 'status'])
-    ->middleware('scopes:user');
+// 获取，创建或更新状态
+Route::match(['get', 'post'], 'status', [UserController::class, 'status'])
+    ->middleware('scopes:profile');
 
 // Route::post('login', [UserController::class, 'fastLogin'])->middleware('scopes:login');
