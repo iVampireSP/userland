406817a85d477c4fca082e3ea250c87a4601471b
Twilight <im@ivampiresp.com>
2024-06-28 19:30:48 +0800

    增加 pgvector，默认 pgsql

---
+++ database/migrations/2000_08_03_000000_create_vector_extension.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        $sql = 'CREATE EXTENSION IF NOT EXISTS vector';
+        echo "\nYou can run $sql manually as root. you must run as root!\n";
+        DB::statement($sql);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        DB::statement('DROP EXTENSION vector');
+    }
+};
