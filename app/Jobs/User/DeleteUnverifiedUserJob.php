3e2742799884f1553ca898c39a14c128d5e98c40
Twilight <im@ivampiresp.com>
2024-07-06 19:58:13 +0800

    改进 定时删除逻辑

--- app/Jobs/User/DeleteUnverifiedUserJob.php
+++ app/Jobs/User/DeleteUnverifiedUserJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs\User;
 
+use App\Jobs\UserDeleteJob;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -16,6 +17,10 @@ class DeleteUnverifiedUserJob implements ShouldQueue
     public function handle(): void
     {
         // 删除注册时间超过 3 天的未验证邮箱或手机号的用户。
-        (new User)->whereNull('email_verified_at')->whereNull('phone_verified_at')->where('created_at', '<', now()->subDays(3))->delete();
+        (new User)->whereNull('email_verified_at')->whereNull('phone_verified_at')->where('created_at', '<', now()->subDays(3))->chunk(100, function ($users) {
+            $users->each(function ($user) {
+                self::dispatch(new UserDeleteJob($user));
+            });
+        });
     }
 }
