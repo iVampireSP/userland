a56855810f69283a51aebb3cb8759ca3b2c12e85
Twilight <im@ivampiresp.com>
2024-07-11 21:24:33 +0800

    回滚 授权页面

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -11,21 +11,71 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
+<body style="height: 100vh">
+<div class="d-flex justify-content-center align-items-center" style="height:100%">
+    <div>
+        @if (!$client->trusted)
+            <div class="container">
+                <div class="row">
+                    <div>
 
-<body>
+                        <h1>授权请求</h1>
+                        <p><strong>{{ $client->name }}</strong> 正在申请访问您的账户。</p>
 
-@if ($client->trusted)
-    @include('passport::trusted')
-@else
-    @include('passport::standard')
-@endif
+                        <!-- Scope List -->
+                        @if (count($scopes) > 0)
+                            <div class="scopes">
+                                <p><strong>此应用程序将被允许: </strong></p>
+
+                                <ul>
+                                    @foreach ($scopes as $scope)
+                                        <li>{{ $scope->description }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <div class="buttons">
+                            <button class="btn btn-success btn-approve" onclick="accept()">授权</button>
+
+                            <button class="btn btn-danger" onclick="deny()">取消</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+    @else
+
+        <h1>正在继续...</h1>
+
+        <div style="width: 100%;" class="text-center mt-5">
+            <!-- spin -->
+            <div class="spinner-border text-primary" role="status">
+                <span class="visually-hidden">正在继续</span>
+            </div>
+        </div>
+
+        <script>
+            setTimeout(() => {
+                accept()
+            }, 100)
+        </script>
+
+    @endif
+
+</div>
+
 
 <!-- Authorize Button -->
 <form class="d-none" method="post" action="{{ route('passport.authorizations.approve') }}" id="authorize-form">
     @csrf
+
     <input type="hidden" name="state" value="{{ $request->state }}">
     <input type="hidden" name="client_id" value="{{ $client->getKey() }}">
     <input type="hidden" name="auth_token" value="{{ $authToken }}">
+
+
 </form>
 
 <!-- Cancel Button -->
