3ae5ff39e90e735a6e7e33e7a563fb716b2ba499
Twilight <im@ivampiresp.com>
2024-07-11 23:01:10 +0800

    修复 排版问题

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -60,19 +60,20 @@
             </div>
         </div>
     @else
+        <div>
+            <h1>正在继续...</h1>
 
-        <h1>正在继续...</h1>
-
-        <div style="width: 100%;" class="text-center mt-5">
-            <!-- spin -->
-            <div class="spinner-border text-primary" role="status">
-                <span class="visually-hidden">正在继续</span>
+            <div style="width: 100%;" class="text-center mt-5">
+                <!-- spin -->
+                <div class="spinner-border text-primary" role="status">
+                    <span class="visually-hidden">正在继续</span>
+                </div>
             </div>
         </div>
 
         <script>
             setTimeout(() => {
-                accept()
+                // accept()
             }, 100)
         </script>
 
