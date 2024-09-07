34c9189c960cccb66b1daea4d66ffad05f6bfa83
ivamp <im@ivampiresp.com>
2024-09-07 18:11:57 +0800

    修复 邮箱注册后不能立即发送注册邮件的问题

--- app/Http/Controllers/Web/Auth/RegisterController.php
+++ app/Http/Controllers/Web/Auth/RegisterController.php
@@ -37,7 +37,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data): User
     {
-        $u =  (new User)->createOrRestore([
+        $u = (new User)->createOrRestore([
             'name' => $data['name'] ?? null,
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
